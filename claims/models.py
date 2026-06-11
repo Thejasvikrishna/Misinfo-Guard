0f755d718a63a97cc6224eb9b2de153910e15cbd
Sumith Mogaveera <sumithkanchan247@gmail.com>
2026-06-11 23:31:30 +0530

    feat(models) ; create claim model

---
+++ claims/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Claim(models.Model):
+    TYPES = [('text', 'Text'), ('image', 'Image'), ('url', 'URL')]
+    STATUS = [
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed'),
+        ('ERROR', 'Error'),
+    ]
+
+    is_correct = models.BooleanField(null=True, blank=True)
+    claim_text = models.TextField(blank=True, null=True)
+    sources = models.JSONField(default=list)
+    claim_type = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='claims/', null=True, blank=True)
+    status = models.CharField(max_length=20, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+    timeframe = models.CharField(max_length=20, default='current')
