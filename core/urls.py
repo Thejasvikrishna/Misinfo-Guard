71688fa87825a61b6f71777f4fd08985b9af38c1
Sumith Mogaveera <sumithkanchan247@gmail.com>
2026-06-12 18:46:16 +0530

    feat(routes): configure API router

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from verifier.views import ClaimViewSet
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+router = DefaultRouter()
+router.register(r'claims', ClaimViewSet)
