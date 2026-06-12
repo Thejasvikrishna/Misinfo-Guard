c1d435070dba667b4b36810532fa064e5c8c413c
Sumith Mogaveera <sumithkanchan247@gmail.com>
2026-06-12 18:48:16 +0530

    feat(routes) :initial commands

--- core/urls.py
+++ core/urls.py
@@ -25,3 +25,10 @@ urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from verifier.views import ClaimViewSet
+from verifier.views import home
+from django.conf import settings
+from django.conf.urls.static import static
